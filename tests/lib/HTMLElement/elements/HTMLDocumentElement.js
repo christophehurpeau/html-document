727e2108d34574d87482461fe6d8ae7047817b0f
Egor Bolgov <e.bolgov@semrush.com>
2016-07-18 12:19:25 +0300

    Code style fixed.
    Signed-off-by: Egor Bolgov <e.bolgov@semrush.com>

--- tests/lib/HTMLElement/elements/HTMLDocumentElement.js
+++ tests/lib/HTMLElement/elements/HTMLDocumentElement.js
@@ -1,36 +1,26 @@
-/*global test*/
+/* global test */
 'use strict';
-
-var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
 var _proclaim = require('proclaim');
 
-var _proclaim2 = _interopRequireDefault(_proclaim);
-
 var _fs = require('fs');
 
-var expect = _proclaim2['default'].strictEqual;
-var throws = _proclaim2['default'].throws;
-
 var lib = '../../../../lib/';
-
 var Document = require(lib + 'Document');
-var Doctype = require(lib + 'Doctype');
-
 var fileText = (0, _fs.readFileSync)(__dirname + '/../../../data/linkstest.html', { encoding: 'utf-8' });
 
 test('should popup HTMLDocumentElement if imported through document.documentElement.innerHTML', function () {
     var document = new Document();
     document.location = 'http://www.some.url/and-page';
     document.documentElement.innerHTML = fileText;
-    expect(document.documentElement.children[0].tagName, 'head');
+    (0, _proclaim.strictEqual)(document.documentElement.children[0].tagName, 'head');
 });
 
 test('should do same as above on copy', function () {
     var document = new Document();
     document.location = 'http://www.some.url/and-page';
     document.documentElement.innerHTML = fileText;
-    var bodyClone = document.body.cloneNode(true);
-    expect(document.documentElement.children[0].tagName, 'head');
+    var documentClone = document.documentElement.cloneNode(true);
+    (0, _proclaim.strictEqual)(documentClone.children[0].tagName, 'head');
 });
 //# sourceMappingURL=HTMLDocumentElement.js.map
--- tests/src/HTMLElement/elements/HTMLDocumentElement.js
+++ tests/src/HTMLElement/elements/HTMLDocumentElement.js
@@ -1,29 +1,23 @@
-/*global test*/
-import assert from 'proclaim';
+/* global test */
+import {strictEqual} from 'proclaim';
 import {readFileSync} from 'fs';
 
-const expect = assert.strictEqual;
-const throws = assert.throws;
-
 const lib = '../../../../lib/';
-
 const Document = require(lib + 'Document');
-const Doctype = require(lib + 'Doctype');
-
 const fileText = readFileSync(__dirname + '/../../../data/linkstest.html', { encoding: 'utf-8' });
 
 test('should popup HTMLDocumentElement if imported through document.documentElement.innerHTML', () => {
     let document = new Document();
     document.location = 'http://www.some.url/and-page';
     document.documentElement.innerHTML = fileText;
-    expect(document.documentElement.children[0].tagName, 'head');
+    strictEqual(document.documentElement.children[0].tagName, 'head');
 });
 
 test('should do same as above on copy', () => {
     let document = new Document();
     document.location = 'http://www.some.url/and-page';
     document.documentElement.innerHTML = fileText;
-    let bodyClone = document.body.cloneNode(true);
-    expect(document.documentElement.children[0].tagName, 'head');
+    let documentClone = document.documentElement.cloneNode(true);
+    strictEqual(documentClone.children[0].tagName, 'head');
 });
 
