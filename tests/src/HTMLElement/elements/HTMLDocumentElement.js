7664d5ff50f8dda4ea83f8b22387a1f4db295adc
Egor Bolgov <e.bolgov@semrush.com>
2016-07-18 12:53:44 +0300

    Updated tests.
    Signed-off-by: Egor Bolgov <e.bolgov@semrush.com>

--- tests/src/HTMLElement/elements/HTMLDocumentElement.js
+++ tests/src/HTMLElement/elements/HTMLDocumentElement.js
@@ -1,23 +1,42 @@
 /* global test */
 import {strictEqual} from 'proclaim';
-import {readFileSync} from 'fs';
 
 const lib = '../../../../lib/';
 const Document = require(lib + 'Document');
-const fileText = readFileSync(__dirname + '/../../../data/linkstest.html', { encoding: 'utf-8' });
 
 test('should popup HTMLDocumentElement if imported through document.documentElement.innerHTML', () => {
     let document = new Document();
-    document.location = 'http://www.some.url/and-page';
-    document.documentElement.innerHTML = fileText;
-    strictEqual(document.documentElement.children[0].tagName, 'head');
+    document.documentElement.innerHTML = '<html></html>';
+    strictEqual(document.documentElement.children.length, 0);
+});
+
+test('should popup HTMLDocumentElement children if imported through document.documentElement.innerHTML', () => {
+    let document = new Document();
+    document.documentElement.innerHTML = '<html><head></head><body>test</body></html>';
+    strictEqual(document.documentElement.children.length, 2);
+    strictEqual(document.body.innerHTML, 'test');
+});
+
+test('should popup HTMLDocumentElement values if imported through document.documentElement.innerHTML', () => {
+    let document = new Document();
+    document.documentElement.innerHTML = '<html><body>test</body></html>';
+    strictEqual(document.documentElement.children.length, 1);
+    strictEqual(document.head.innerHTML, '');
+});
+
+test('should not popup HTMLDocumentElement children if no <html> in content', () => {
+    let document = new Document();
+    document.documentElement.innerHTML = '<head><title>test1</title></head><body>test</body>';
+    strictEqual(document.documentElement.children.length, 2);
+    strictEqual(document.head.innerHTML, '<title>test1</title>');
+    strictEqual(document.body.innerHTML, 'test');
 });
 
 test('should do same as above on copy', () => {
     let document = new Document();
-    document.location = 'http://www.some.url/and-page';
-    document.documentElement.innerHTML = fileText;
+    document.documentElement.innerHTML = '<html><head></head><body>test</body></html>';
     let documentClone = document.documentElement.cloneNode(true);
-    strictEqual(documentClone.children[0].tagName, 'head');
+    strictEqual(documentClone.children.length, 2);
+    strictEqual(documentClone.getElementsByTagName('body')[0].innerHTML, 'test');
 });
 
